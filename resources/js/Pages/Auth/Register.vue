23aa1f154fe5d16e3adb146959fe39b1154e34ef
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-24 11:20:15 +0200

    Add AuthController and create register method with form helper from Inertia.

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,16 +1,19 @@
 <script setup>
 import { reactive } from 'vue';
+import { useForm } from '@inertiajs/vue3';
 
-const form = reactive({
+const form = useForm({
     name: null,
     email: null,
     password: null,
     password_confirmation: null,
-})
+});
 
 const submit = () => {
-    console.log(form);
-}
+    form.post(route('register'), {
+        onError: () => form.reset('password', 'password_confirmation'),
+    });
+};
 </script>
 
 <template>
@@ -24,23 +27,27 @@ const submit = () => {
             <div class="mb-6">
                 <label>Name</label>
                 <input type="text" v-model="form.name" />
+                <small>{{ form.errors.name }}</small>
             </div>
             <div class="mb-6">
                 <label>Email</label>
                 <input type="text" v-model="form.email" />
+                <small>{{ form.errors.email }}</small>
             </div>
             <div class="mb-6">
                 <label>Password</label>
                 <input type="password" v-model="form.password" />
+                <small>{{ form.errors.password }}</small>
             </div>
             <div class="mb-6">
                 <label>Confirm Password</label>
                 <input type="password" v-model="form.password_confirmation" />
+                <small>{{ form.errors.password_confirmation }}</small>
             </div>
 
             <div>
                 <p class="text-slate-600 mb-2">Already a user ? <a href="#" class="text-link">Login</a></p>
-                <button class="primary-btn">Register</button>
+                <button class="primary-btn" :disabled="form.processing">Register</button>
             </div>
         </form>
     </div>
